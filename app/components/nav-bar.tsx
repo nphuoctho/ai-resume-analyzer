e2e3ac7a452ae252d7c12d3878d2200b92215016
nphuoctho <nguyenphuoctho0310@gmail.com>
2025-11-28 18:32:16 +0700

    chore: update login redirect

--- app/components/nav-bar.tsx
+++ app/components/nav-bar.tsx
@@ -1,14 +1,25 @@
-import { Link } from "react-router";
+import { Link } from 'react-router';
+import { usePuterStore } from '~/lib/puter';
 
 const Navbar = () => {
+  const { auth } = usePuterStore();
+
   return (
-    <nav className="navbar">
-      <Link to="/">
-        <p className="text-2xl font-bold text-gradient uppercase">resumind</p>
+    <nav className='navbar'>
+      <Link to='/'>
+        <p className='text-2xl font-bold text-gradient uppercase'>resumind</p>
       </Link>
-      <Link to="/upload" className="primary-button w-fit">
-        Upload Resume
-      </Link>
+      <div className='flex flex-row items-center justify-between gap-4'>
+        <Link to='/upload' className='primary-button w-fit'>
+          Upload Resume
+        </Link>
+
+        {auth.isAuthenticated && (
+          <button className='primary-button w-fit' onClick={auth.signOut}>
+            Logout
+          </button>
+        )}
+      </div>
     </nav>
   );
 };
